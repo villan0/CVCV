f9263b7b29a1813d9766f0122d714cd2b5832055
EmanSaputra <76545003+EmanSaputra@users.noreply.github.com>
2021-04-18 23:46:11 +0700

    Menambahkan Fitur Wallpaper

--- start.bat
+++ start.bat
@@ -11,6 +11,8 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bmp /f 
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters  
 echo Berhasil Menginstall!, Jika RDPnya Mati Silahkan Rebuild Lagi!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
